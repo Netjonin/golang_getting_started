8145fa7eacf6cbfec22f9a42c9670cc30ae75434
Netjonin <pencilpepeyo@yahoo.com>
2022-06-23 10:29:01 +0100

    map and backward compatibilty added

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,23 +1,35 @@
 package greetings
 
 import (
-    "errors"
-    "fmt"
+	"errors"
+	"fmt"
 	"math/rand"
 	"time"
 )
 
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
-    // If no name was given, return an error with a message.
-    if name == "" {
-        return name, errors.New("empty name")
-    }
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return name, errors.New("empty name")
+	}
 
-    // If a name was received, return a value that embeds the name
-    // in a greeting message.
-    message := fmt.Sprintf(randomFormat(), name)
-    return message, nil
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
 }
 
 func init() {
--- hello/hello.go
+++ hello/hello.go
@@ -12,9 +12,11 @@ func main() {
 	log.SetPrefix("greeting to humans: ")
 	log.SetFlags(0)
 	message, err := greetings.Hello("Ayofe")
+	messages, err := greetings.Hellos([]string{"Ayofe", "Ajike", "Ayinde"})
 
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(message)
+	fmt.Println(messages)
 }
